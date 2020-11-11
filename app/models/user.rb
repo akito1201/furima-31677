4c40324f2cb6887de4c493ca1d37a70989445584
akito1201 <akito.tennis@gmail.com>
2020-11-11 21:21:47 +0900

    ユーザー管理機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+      validates :last_name, presence: true
+      validates :first_name, presence: true
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/ } do
+      validates :kana_last_name, presence: true
+      validates :kana_first_name, presence: true
+  end
 end
