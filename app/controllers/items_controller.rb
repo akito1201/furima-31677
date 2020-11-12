813917a6252e0fc95b617a01b4c02ce62f2c64f6
akito1201 <akito.tennis@gmail.com>
2020-11-12 12:28:41 +0900

    newアクション、createアクションのルーティング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,24 @@
 class ItemsController < ApplicationController
- 
+ before_action :authenticate_user!, except: [:index, :show]
   def index
   end
+
+  def new
+    @item = Item.new
+  end
   
+  def create
+    @item = Item.new(item_paramter)
+    if @item.save
+      redirect_to: root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_paramter
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_id, :region_id, :days_id, :price).merge(user_id: current_user.id)
+  end
 end
