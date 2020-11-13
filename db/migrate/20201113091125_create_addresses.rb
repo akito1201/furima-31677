c51f9fc53f92d91333f7940eecf97e8531849738
akito1201 <akito.tennis@gmail.com>
2020-11-13 18:16:38 +0900

    RecordとAddressモデル作成

---
+++ db/migrate/20201113091125_create_addresses.rb
@@ -0,0 +1,13 @@
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.integer :zipcode, null: false
+      t.integer :prefecture_id, null: false
+      t.string :city, null: false
+      t.string :house, null: false
+      t.integer :telephone, null: false
+      t.references :record, foreign_key: true
+      t.timestamps
+    end
+  end
+end
