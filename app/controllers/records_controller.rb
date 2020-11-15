6bc3b8894e80dca4e1f83d0466e6b2134ed01d05
akito1201 <akito.tennis@gmail.com>
2020-11-15 18:37:46 +0900

    クレジットカード機能実装

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -3,16 +3,17 @@ class RecordsController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-    @record = Record.new
+    @user_record = UserRecord.new
     redirect_to root_path if current_user.id == @item.user.id
   end
 
   def create
     @item = Item.find(params[:item_id])
-    @record = Record.new(record_params)
-    if @record.valid?
+    @user_record = UserRecord.new(user_record_params)
+
+    if @user_record.valid?
       pay_item
-      @record.save
+      @user_record.save
       return redirect_to root_path
     else
       render 'index'
@@ -21,15 +22,15 @@ class RecordsController < ApplicationController
 
   private
 
-  def record_params
-    params.require(:record).permit(:time).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
+  def user_record_params
+    params.require(:user_record).permit(:zipcode, :region_id, :city, :house, :building, :telephone).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
   end
 
   def pay_item
     Payjp.api_key = "sk_test_0f8309f57450f76854fd1ebb"
     Payjp::Charge.create(
-      amount: @record.item.price,
-      card: record_params[:token],
+      amount: @item.price,
+      card: user_record_params[:token],
       currency: 'jpy'
     )
   end
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -3,17 +3,17 @@ const pay = () => {
   const form = document.getElementById("charge-form");
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    
     const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get("record[number]"),
-      cvc: formData.get("record[cvc]"),
-      exp_month: formData.get("record[exp_month]"),
-      exp_year: `20${formData.get("record[exp_year]")}`,
+      number: formData.get("user_record[number]"),
+      cvc: formData.get("user_record[cvc]"),
+      exp_month: formData.get("user_record[exp_month]"),
+      exp_year: `20${formData.get("user_record[exp_year]")}`,
     };
-
+    
+//tokenが発行できたらHTMLに埋め込み
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
@@ -21,12 +21,13 @@ const pay = () => {
         const tokenObj = `<input value=${token} name='token' type="hidden">`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
-
+//クレジットカードの情報消去
       document.getElementById("card-number").removeAttribute("name");
       document.getElementById("card-exp-month").removeAttribute("name");
       document.getElementById("card-exp-year").removeAttribute("name");
       document.getElementById("card-cvc").removeAttribute("name");
 
+//再度送信ボタンを押す（preventDefaultがかかってるため)
       document.getElementById("charge-form").submit();
     });
   });
